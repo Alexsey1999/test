902302b2528aaa7a01b507544889cbc921f9bc5e
Alexsey Shishkin <shishkinalexsey42@gmail.com>
2021-10-11 12:15:31 +0300

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+const getData = async () => {
+    const requestData = new FormData()
+
+    requestData.append('name', 'al')
+
+    const resp = await fetch('https://swapi.dev/api/planets', {
+        method: 'POST',
+        body: requestData
+    })
+
+    const data = await resp.json()
+
+    console.log(data, 123)
+}
+
+getData()
